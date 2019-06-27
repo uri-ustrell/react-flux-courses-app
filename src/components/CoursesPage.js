3bb85fb0c50a4adb41b7cc2e7fb4fda2e199ffbb
root <root@DESKTOP-QRLQ7Q8.localdomain>
2019-06-27 17:45:24 +0200

    Controller Views: split logic components and view components

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,40 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
 
 function CoursesPage(props) {
 	const [courses, setCourses] = useState([]);
 
 	useEffect(() => {
-		const effect = async () => {
+		(async () => {
 			const data = await getCourses();
 			setCourses(data);
-		};
-		effect();
-	},[]);
+		})();
+	}, []);
 
 	return (
 		<>
 			<h2>Courses</h2>
-			<table className="table">
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Author ID</th>
-						<th>Category</th>
-					</tr>
-				</thead>
-				<tbody>
-					{courses.map(course => {
-						return (
-							<tr key={course.id}>
-								<td>{course.title}</td>
-								<td>{course.authorId}</td>
-								<td>{course.category}</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
+			<CourseList courses={courses} />
 		</>
 	);
 }
